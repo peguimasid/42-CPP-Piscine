b22f90a24e77ca256d27dc84bb8746e28e85bbc2
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-24 18:40:56 -0300

    module00 ex00 done

--- module-00/ex00/megaphone.cpp
+++ module-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:13:02 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/24 18:00:53 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/24 18:39:33 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 using namespace std;
 
-int main(void) {
-  cout << "Hello World" << endl;
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+    return 0;
+  };
+  for (int i = 1; i < argc; i++) {
+    string str = argv[i];
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    cout << str;
+  }
+  cout << endl;
+  return 0;
 }
