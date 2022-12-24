5abd0a0efd68a81cd031a0a19367ebbca48f6233
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-24 18:49:08 -0300

    module00 ex00 done - using pdf requirements

--- module-00/ex00/megaphone.cpp
+++ module-00/ex00/megaphone.cpp
@@ -6,24 +6,22 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:13:02 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/24 18:39:33 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/24 18:48:37 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char **argv) {
   if (argc == 1) {
-    cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
-    return 0;
-  };
-  for (int i = 1; i < argc; i++) {
-    string str = argv[i];
-    transform(str.begin(), str.end(), str.begin(), ::toupper);
-    cout << str;
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
   }
-  cout << endl;
+  for (int i = 1; i < argc; i++) {
+    int j = 0;
+    while (argv[i][j]) {
+      std::cout << char(toupper(argv[i][j++]));
+    }
+  }
+  std::cout << std::endl;
   return 0;
 }
