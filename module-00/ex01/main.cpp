893a8cf6b6304ad734f10ec0aeb7388ca9eaeff2
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-24 23:42:02 -0300

    get user input

--- module-00/ex01/main.cpp
+++ module-00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:13:02 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/24 23:23:26 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/24 23:41:27 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 #include "./includes/Contact.hpp"
 
 void printUsage() {
+  std::cout << "\033[0;34m";
   std::cout << "Available commands:" << std::endl;
   std::cout << "- ADD: add a contact to phonebook" << std::endl;
   std::cout << "- SEARCH: search for a contact in the phonebook" << std::endl;
   std::cout << "- EXIT: quit the program" << std::endl;
+  std::cout << "\033[0m\n";
 }
 
 std::string getInputString() {
@@ -30,12 +32,24 @@ std::string getInputString() {
 
   return result;
 }
+bool isValidInput(std::string input) {
+  return input == "ADD" || input == "SEARCH" || input == "EXIT";
+}
+
+void addContact() {
+}
+
+void searchContact() {
+}
 
 void inputLoop() {
   while (true) {
     std::string input = getInputString();
 
-    std::cout << input << std::endl;
+    if (!isValidInput(input)) printUsage();
+    if (input == "ADD") addContact();
+    if (input == "SEARCH") searchContact();
+    if (input == "EXIT") break;
   }
 }
 
