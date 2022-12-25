f973f386cb2ea9aaa3082ccedbcad3b9dfe178b0
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-24 23:23:44 -0300

    buildin program

--- module-00/ex01/main.cpp
+++ module-00/ex01/main.cpp
@@ -6,16 +6,40 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:13:02 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/24 23:07:18 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/24 23:23:26 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 #include "./includes/Contact.hpp"
 
+void printUsage() {
+  std::cout << "Available commands:" << std::endl;
+  std::cout << "- ADD: add a contact to phonebook" << std::endl;
+  std::cout << "- SEARCH: search for a contact in the phonebook" << std::endl;
+  std::cout << "- EXIT: quit the program" << std::endl;
+}
+
+std::string getInputString() {
+  std::string result;
+
+  std::cout << "Enter a command: ";
+  std::cin >> result;
+
+  return result;
+}
+
+void inputLoop() {
+  while (true) {
+    std::string input = getInputString();
+
+    std::cout << input << std::endl;
+  }
+}
+
 int main(void) {
-  Contact myContact;
-  myContact.setFirstName("Guilhermo");
-  std::cout << myContact.getFirstName() << std::endl;
+  printUsage();
+  inputLoop();
 }
--- module-00/ex01/src/Contact.cpp
+++ module-00/ex01/src/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 22:37:38 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/24 23:05:23 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/24 23:12:03 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,38 @@ void Contact::setFirstName(std::string value) {
   this->_first_name = value;
 }
 
+void Contact::setLastName(std::string value) {
+  this->_last_name = value;
+}
+
+void Contact::setNickname(std::string value) {
+  this->_nickname = value;
+}
+
+void Contact::setPhoneNumber(std::string value) {
+  this->_phone_number = value;
+}
+
+void Contact::setDarkestSecret(std::string value) {
+  this->_darkest_secret = value;
+}
+
 std::string Contact::getFirstName() {
   return this->_first_name;
+}
+
+std::string Contact::getLastName() {
+  return this->_last_name;
+}
+
+std::string Contact::getNickname() {
+  return this->_nickname;
+}
+
+std::string Contact::getPhoneNumber() {
+  return this->_phone_number;
+}
+
+std::string Contact::getDarkestSecret() {
+  return this->_darkest_secret;
 }
