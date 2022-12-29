e69baad1813113e13e5e30e9205966dd7bf47dfc
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-29 18:51:41 -0300

    finish ex04

--- module-01/ex04/main.cpp
+++ module-01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 16:50:30 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/29 17:54:17 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/29 18:44:41 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@ int error(std::string errorMessage) {
   std::cout << errorMessage << std::endl;
   std::cout << "\033[0m";
   return 1;
+}
+
+std::string replaceLine(std::string currentLine, std::string s1, std::string s2) {
+  std::string result = currentLine;
+
+  int foundIndex = result.find(s1);
+  while (foundIndex >= 0) {
+    result = result.substr(0, foundIndex) + s2 + result.substr(foundIndex + s1.size(), result.size() - 1);
+    foundIndex = result.find(s1);
+  }
+
+  return result;
 }
 
 int main(int argc, char **argv) {
@@ -38,12 +50,18 @@ int main(int argc, char **argv) {
   if (inputFile.peek() == std::ifstream::traits_type::eof()) {
     return error("File is empty");
   }
+  if (s1 == s2) {
+    return error("s1 is equal to s2");
+  }
+  if (s1.size() == 0 || s2.size() == 0) {
+    return error("Strings cannot be empty");
+  }
 
   std::ofstream outputFile(filename + ".replace");
   std::string currentLine;
 
   while (getline(inputFile, currentLine)) {
-    outputFile << currentLine << std::endl;
+    outputFile << replaceLine(currentLine, s1, s2) << std::endl;
   }
 
   inputFile.close();
