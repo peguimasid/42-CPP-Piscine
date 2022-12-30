aa89078a36fc1645b5b94ae6f9df05727bd555ba
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-30 17:44:29 -0300

    create harl.hpp clas

---
+++ module-01/ex05/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/30 17:42:38 by gmasid            #+#    #+#             */
+/*   Updated: 2022/12/30 17:44:08 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_H
+#define HARL_H
+
+#include <iostream>
+#include <string>
+
+class Harl {
+ private:
+  void debug(void);
+  void info(void);
+  void warning(void);
+  void error(void);
+
+ public:
+  void complain(std::string level);
+};
+
+#endif
