53eb52590840baf59db34abd12f95632ef0c93aa
Guilhermo Masid <guilhermomasid@gmail.com>
2023-01-04 18:13:30 -0300

    copy folder 01 to ex02 and add functions getrawbits and setrawbits

---
+++ module-02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 16:30:42 by gmasid            #+#    #+#             */
+/*   Updated: 2023/01/03 19:19:41 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void) {
+  Fixed a;
+  Fixed const b(10);
+  Fixed const c(42.42f);
+  Fixed const d(b);
+  a = Fixed(1234.4321f);
+  std::cout << "a is " << a << std::endl;
+  std::cout << "b is " << b << std::endl;
+  std::cout << "c is " << c << std::endl;
+  std::cout << "d is " << d << std::endl;
+  std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+  return 0;
+}
