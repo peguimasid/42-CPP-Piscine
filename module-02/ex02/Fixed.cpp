bb7275abaeb506802f779f3173644c1b816ce821
Guilhermo Masid <guilhermomasid@gmail.com>
2023-01-07 18:12:28 -0300

    change name from comparison to arithmetic

--- module-02/ex02/Fixed.cpp
+++ module-02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 18:02:48 by gmasid            #+#    #+#             */
-/*   Updated: 2023/01/07 18:10:33 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/01/07 18:12:12 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ bool Fixed::operator!=(const Fixed& fixed) {
   return this->_value != fixed._value;
 }
 
-// Comparison operators
+// Arithmetic operators
 Fixed Fixed::operator+(const Fixed& other) {
   Fixed result;
   result.setRawBits(this->_value + other.getRawBits());
