ad8251be6e927095e742963f576649480ed25b93
Guilhermo Masid <guilhermomasid@gmail.com>
2023-01-11 18:52:37 -0300

    add copy constructor

--- module-03/ex00/ClapTrap.cpp
+++ module-03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 18:43:46 by gmasid            #+#    #+#             */
-/*   Updated: 2023/01/11 18:45:24 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/01/11 18:52:21 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,17 @@ ClapTrap::ClapTrap(std::string name) {
   this->_hitPoints = 10;
   this->_energyPoints = 10;
   this->_attackDamage = 0;
+  std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other) {
+  this->_name = other._name;
+  this->_hitPoints = other._hitPoints;
+  this->_energyPoints = other._energyPoints;
+  this->_attackDamage = other._attackDamage;
+  std::cout << "Copy constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap() {
+  std::cout << "Destructor called" << std::endl;
 }
