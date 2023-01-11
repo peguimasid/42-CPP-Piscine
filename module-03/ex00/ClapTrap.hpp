847cbf957912e9b752104e7e6f32ee3098c063cf
Guilhermo Masid <guilhermomasid@gmail.com>
2023-01-11 18:39:06 -0300

    create class for clap trap

---
+++ module-03/ex00/ClapTrap.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 18:31:58 by gmasid            #+#    #+#             */
+/*   Updated: 2023/01/11 18:37:46 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAP_TRAP_HPP
+#define CLAP_TRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+ private:
+  std::string _name;
+  unsigned int _hitPoints;
+  unsigned int _energyPoints;
+  unsigned int _attackDamage;
+
+ public:
+  // Constructors and Destructors
+  ClapTrap();
+  ClapTrap(const ClapTrap& other);
+  ~ClapTrap();
+
+  // Copy assignment operator
+  ClapTrap& operator=(const ClapTrap& other);
+
+  void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+};
+
+#endif
