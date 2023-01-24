74a1e555d88aa1889673de6b8dec684d1d03b3fe
Guilhermo Masid <guilhermomasid@gmail.com>
2023-01-24 14:01:16 -0300

    add test main to wronganimal and wrong cat

--- module-04/ex00/main.cpp
+++ module-04/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "./includes/Animal.hpp"
 #include "./includes/Cat.hpp"
 #include "./includes/Dog.hpp"
+#include "./includes/WrongAnimal.hpp"
+#include "./includes/WrongCat.hpp"
 
 int main(void) {
   const Animal* meta = new Animal();
@@ -8,7 +10,7 @@ int main(void) {
   const Animal* i = new Cat();
   std::cout << j->getType() << " " << std::endl;
   std::cout << i->getType() << " " << std::endl;
-  i->makeSound();  // will output the cat sound!
+  i->makeSound();
   j->makeSound();
   meta->makeSound();
   delete meta;
@@ -16,3 +18,12 @@ int main(void) {
   delete j;
   return 0;
 }
+
+// int main(void) {
+//   // const Animal* one = new Cat();
+//   const WrongAnimal* one = new WrongCat();
+//   std::cout << one->getType() << " " << std::endl;
+//   one->makeSound();
+//   delete one;
+//   return 0;
+// }
