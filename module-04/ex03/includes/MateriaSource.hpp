b3f2b18a4360eb54b828f3a038fcb4b780d8e1b4
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-04 14:49:39 -0300

    create MateriaSource class

---
+++ module-04/ex03/includes/MateriaSource.hpp
@@ -0,0 +1,22 @@
+#ifndef MATERIA_SOURCE_HPP
+#define MATERIA_SOURCE_HPP
+
+#include "IMateriaSource.hpp"
+
+class MateriaSource : public IMateriaSource {
+ private:
+  AMateria* _materia[4];
+
+ public:
+  // Constructors and Destructors
+  MateriaSource();
+  MateriaSource(const MateriaSource& other);
+  ~MateriaSource();
+  MateriaSource& operator=(const MateriaSource& other);
+
+  // Member Functions
+  void learnMateria(AMateria*);
+  AMateria* createMateria(std::string const& type);
+};
+
+#endif
