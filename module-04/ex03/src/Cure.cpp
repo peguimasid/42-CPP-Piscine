f33405f7bfcfe180b499b5dd24fe5fde559ea975
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-04 16:01:10 -0300

    fix: Cure and Ice cure use function declaration

--- module-04/ex03/src/Cure.cpp
+++ module-04/ex03/src/Cure.cpp
@@ -15,6 +15,6 @@ AMateria* Cure::clone() const {
   return new Cure(*this);
 }
 
-void use(ICharacter& target) {
+void Cure::use(ICharacter& target) {
   std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
 }
--- module-04/ex03/src/Ice.cpp
+++ module-04/ex03/src/Ice.cpp
@@ -15,6 +15,6 @@ AMateria* Ice::clone() const {
   return new Ice(*this);
 }
 
-void use(ICharacter& target) {
+void Ice::use(ICharacter& target) {
   std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
 }
