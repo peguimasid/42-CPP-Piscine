5fe50bfd418beca70cfeb2c8a61d07b4f84f5eda
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-04 15:27:18 -0300

    fix makefile

--- module-04/ex03/main.cpp
+++ module-04/ex03/main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+#include "./includes/AMateria.hpp"
+#include "./includes/Character.hpp"
+#include "./includes/Cure.hpp"
+#include "./includes/IMateriaSource.hpp"
+#include "./includes/Ice.hpp"
+#include "./includes/MateriaSource.hpp"
 
 int main() {
-  std::cout << "Hello World" << std::endl;
+  IMateriaSource* src = new MateriaSource();
+  src->learnMateria(new Ice());
+  src->learnMateria(new Cure());
+  ICharacter* me = new Character("me");
+  AMateria* tmp;
+  tmp = src->createMateria("ice");
+  me->equip(tmp);
+  tmp = src->createMateria("cure");
+  me->equip(tmp);
+  ICharacter* bob = new Character("bob");
+  me->use(0, *bob);
+  me->use(1, *bob);
+  delete bob;
+  delete me;
+  delete src;
+  return 0;
 }
