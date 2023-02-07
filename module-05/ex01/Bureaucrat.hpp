be222dc18ee38e36219956ca6d557b3ef70fe386
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-07 14:27:54 -0300

    create files to ex01 module 05

---
+++ module-05/ex01/Bureaucrat.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+class Bureaucrat {
+ private:
+  const std::string _name;
+  int _grade;
+
+ public:
+  // Constructors
+  Bureaucrat();
+  Bureaucrat(const std::string name, int grade);
+  Bureaucrat(const Bureaucrat &other);
+  ~Bureaucrat();
+  Bureaucrat &operator=(const Bureaucrat &other);
+
+  // Member functions
+  const std::string getName() const;
+  int getGrade() const;
+  void incrementGrade();
+  void decrementGrade();
+
+  // Exceptions
+  class GradeTooHighException : public std::exception {
+   public:
+    virtual const char *what() const throw();
+  };
+  class GradeTooLowException : public std::exception {
+   public:
+    virtual const char *what() const throw();
+  };
+};
+
+std::ostream &operator<<(std::ostream &output, const Bureaucrat &bureaucrat);
