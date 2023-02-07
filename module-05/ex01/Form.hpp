cad23ae6d081ec869a7d28f452ae06ceda606b2a
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-07 15:06:30 -0300

    add more methods and comments

--- module-05/ex01/Form.hpp
+++ module-05/ex01/Form.hpp
@@ -11,17 +11,31 @@ class Form {
   const int _requiredGradeToExecute;
 
  public:
+  // Constructors
   Form();
+  Form(const std::string &name);
+  Form(const std::string &name, const int gradeToSign, const int gradeToExecute);
   Form(const Form &other);
   ~Form();
   Form &operator=(const Form &other);
 
+  // Getters
   int getRequiredGradeToSign() const;
   int getRequiredGradeToExecute() const;
   bool getIsSigned() const;
   std::string getName() const;
 
+  // Member functions
   void beSigned(Bureaucrat &candidate);
+
+  class GradeTooHighException : public std::exception {
+   public:
+    virtual const char *what() const throw();
+  };
+  class GradeTooLowException : public std::exception {
+   public:
+    virtual const char *what() const throw();
+  };
 };
 
 std::ostream &operator<<(std::ostream &output, const Form &form);
