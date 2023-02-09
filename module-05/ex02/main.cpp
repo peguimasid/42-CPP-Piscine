eb6aa8bd6bf540f9386c80533aa7e3507d47d937
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-09 18:54:04 -0300

    add more tests to main

--- module-05/ex02/main.cpp
+++ module-05/ex02/main.cpp
@@ -1,14 +1,17 @@
+#include "includes/PresidentialPardonForm.hpp"
 #include "includes/RobotomyRequestForm.hpp"
 #include "includes/ShrubberyCreationForm.hpp"
 
 int main() {
   // ShrubberyCreationForm homeForm("home");
   RobotomyRequestForm robotForm("robot");
-  Bureaucrat bu("Gui", 45);
+  PresidentialPardonForm presidentForm("president");
+  Bureaucrat bu("Gui", 5);
 
   try {
     // homeForm.action(bu);
     robotForm.action(bu);
+    presidentForm.action(bu);
   } catch (std::exception &err) {
     std::cerr << "Error: " << err.what() << std::endl;
   }
