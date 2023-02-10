27c5b429cf71c427d966b60f55d204cac50cb5bf
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-10 18:20:23 -0300

    finish and add some tests to main

--- module-05/ex03/main.cpp
+++ module-05/ex03/main.cpp
@@ -1,3 +1,4 @@
+#include "includes/Intern.hpp"
 #include "includes/PresidentialPardonForm.hpp"
 #include "includes/RobotomyRequestForm.hpp"
 #include "includes/ShrubberyCreationForm.hpp"
@@ -7,13 +8,26 @@ int main() {
   RobotomyRequestForm robotForm("robot");
   PresidentialPardonForm presidentForm("president");
   Bureaucrat bu("Gui", 5);
+  Intern intern;
+
+  Form* form1;
+  Form* form2;
+  Form* form3;
 
   try {
     homeForm.action(bu);
     robotForm.action(bu);
     presidentForm.action(bu);
     bu.executeForm(presidentForm);
-  } catch (std::exception &err) {
+    form1 = intern.makeForm("shrubbery creation", "target1");
+    form2 = intern.makeForm("robotomy request", "target2");
+    form3 = intern.makeForm("presidential pardon", "target3");
+    // form3 = intern.makeForm("not exists", "target4");
+
+    form1->action(bu);
+    form2->action(bu);
+    form3->action(bu);
+  } catch (std::exception& err) {
     std::cerr << "Error: " << err.what() << std::endl;
   }
 }
--- module-05/ex03/src/Intern.cpp
+++ module-05/ex03/src/Intern.cpp
@@ -6,16 +6,31 @@
 
 Intern::Intern() {}
 
-Intern::Intern(const Intern &other) {}
+Intern::Intern(const Intern &other) {
+  (void)other;
+}
 
 Intern::~Intern() {}
 
-Intern &Intern::operator=(const Intern &other) {}
-
-// "shrubbery creation",
-// "robotomy request",
-// "presidential pardon",
+Intern &Intern::operator=(const Intern &other) {
+  (void)other;
+  return *this;
+}
 
 Form *Intern::makeForm(const std::string &formName, const std::string &target) const {
-  std::string _forms[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+  if (formName == "shrubbery creation") {
+    return new ShrubberyCreationForm(target);
+  }
+  if (formName == "robotomy request") {
+    return new RobotomyRequestForm(target);
+  }
+  if (formName == "presidential pardon") {
+    return new PresidentialPardonForm(target);
+  }
+
+  throw new NotFoundFormException();
+}
+
+const char *Intern::NotFoundFormException::what() const throw() {
+  return "requested form does not exists";
 }
