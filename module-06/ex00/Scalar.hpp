f7b32e90c2498aacf6d661bd5847358bad9f7632
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-16 12:50:36 -0300

    create convert method

--- module-06/ex00/Scalar.hpp
+++ module-06/ex00/Scalar.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <climits>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
 class Scalar {
  private:
-  /* data */
- public:
   Scalar();
+  Scalar(const Scalar &other);
+  Scalar &operator=(const Scalar &other);
+
+ public:
   ~Scalar();
+
+  void convert(const std::string &literal);
 };
