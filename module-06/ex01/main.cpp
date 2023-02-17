7ee193a896d6ea8f1ddc4eaa4e2fcab3b3a438ba
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-17 11:46:35 -0300

    finish ex01

--- module-06/ex01/main.cpp
+++ module-06/ex01/main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#include "Data.hpp"
 
 int main() {
-  std::cout << "Hello World" << std::endl;
+  Data person;
+
+  person.name = "Guilhermo Masid";
+  person.age = 20;
+
+  std::cout << "Person name is: " << person.name << std::endl;
+  std::cout << "Person age is: " << person.age << std::endl;
+
+  uintptr_t ptr = serialize(&person);
+
+  std::cout << "Ptr is: " << ptr << std::endl;
+
+  Data* newPerson = deserialize(ptr);
+
+  std::cout << "New person name is: " << newPerson->name << std::endl;
+  std::cout << "New person age is: " << newPerson->age << std::endl;
 }
