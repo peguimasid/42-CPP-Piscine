c53e734fb188e470c94aaf89c032da8fb428a498
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-18 13:03:25 -0300

    improve tests

--- module-07/ex00/main.cpp
+++ module-07/ex00/main.cpp
@@ -1,19 +1,19 @@
 #include "whatever.hpp"
 
 int main() {
-  int x = 1;
-  int y = 2;
+  float x = 10.0f;
+  float y = 20.0f;
 
   std::cout << "-----SWAP------" << std::endl;
   std::cout << "x = " << x << std::endl;
   std::cout << "y = " << y << std::endl;
-  swap(x, y);
+  ::swap<float>(x, y);
   std::cout << "x = " << x << std::endl;
   std::cout << "y = " << y << std::endl;
   std::cout << "-----MIN------" << std::endl;
-  std::cout << "min between x and y = " << min(x, y) << std::endl;
+  std::cout << "min between x and y = " << ::min<float>(x, y) << std::endl;
   std::cout << "---------------" << std::endl;
   std::cout << "-----MAX------" << std::endl;
-  std::cout << "max between x and y = " << max(x, y) << std::endl;
+  std::cout << "max between x and y = " << ::max<float>(x, y) << std::endl;
   std::cout << "---------------" << std::endl;
 }
