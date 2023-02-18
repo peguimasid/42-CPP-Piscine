5b6e61583fb17050cee2440b07e5992e5ab789ee
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-18 13:07:19 -0300

    use main from pdf

--- module-07/ex00/main.cpp
+++ module-07/ex00/main.cpp
@@ -1,19 +1,17 @@
 #include "whatever.hpp"
 
-int main() {
-  float x = 10.0f;
-  float y = 20.0f;
-
-  std::cout << "-----SWAP------" << std::endl;
-  std::cout << "x = " << x << std::endl;
-  std::cout << "y = " << y << std::endl;
-  ::swap<float>(x, y);
-  std::cout << "x = " << x << std::endl;
-  std::cout << "y = " << y << std::endl;
-  std::cout << "-----MIN------" << std::endl;
-  std::cout << "min between x and y = " << ::min<float>(x, y) << std::endl;
-  std::cout << "---------------" << std::endl;
-  std::cout << "-----MAX------" << std::endl;
-  std::cout << "max between x and y = " << ::max<float>(x, y) << std::endl;
-  std::cout << "---------------" << std::endl;
+int main(void) {
+  int a = 2;
+  int b = 3;
+  ::swap(a, b);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+  std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+  std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+  return 0;
 }
