f2ee14da82e436733b5c426919e9763868f7a861
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-18 13:29:26 -0300

    implement more tests

--- module-07/ex01/main.cpp
+++ module-07/ex01/main.cpp
@@ -4,8 +4,16 @@ void is_even(const int &num) {
   std::cout << std::boolalpha << (num % 2 == 0) << std::endl;
 }
 
+void say_hello(const std::string &name) {
+  std::cout << "Hello, " << name << " have a good day!" << std::endl;
+}
+
 int main(void) {
   int array[] = {3, 4, 10, 21, 45, 18, 9, 10};
 
   ::iter(array, 8, is_even);
+
+  std::string names[] = {"John", "Karen", "Michael"};
+
+  ::iter(names, 3, say_hello);
 }
