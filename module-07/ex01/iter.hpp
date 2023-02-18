81a572a7189526f7c46e9a81bc097401581ccbcb
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-18 13:27:18 -0300

    implement tests for function

--- module-07/ex01/iter.hpp
+++ module-07/ex01/iter.hpp
@@ -2,7 +2,7 @@
 
 template <typename T>
 void iter(T* array, size_t length, void (*callback)(const T&)) {
-  for (int i = 0; i < length; i++) {
+  for (size_t i = 0; i < length; i++) {
     callback(array[i]);
   }
 }
--- module-07/ex01/main.cpp
+++ module-07/ex01/main.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include "iter.hpp"
+
+void is_even(const int &num) {
+  std::cout << std::boolalpha << (num % 2 == 0) << std::endl;
+}
 
 int main(void) {
-  std::cout << "Hello World" << std::endl;
+  int array[] = {3, 4, 10, 21, 45, 18, 9, 10};
+
+  ::iter(array, 8, is_even);
 }
