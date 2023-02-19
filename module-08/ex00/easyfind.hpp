e6e09eccaad8f4e20fcffb1d7b3fa0f6cb9c7d62
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-19 12:31:07 -0300

    finish function implementation

--- module-08/ex00/easyfind.hpp
+++ module-08/ex00/easyfind.hpp
@@ -1,2 +1,13 @@
 #pragma once
-#include <iostream>
+#include <algorithm>
+
+template <class T>
+typename T::iterator easyfind(T& container, int toFind) {
+  typename T::iterator it = std::find(container.begin(), container.end(), toFind);
+
+  if (it == container.end()) {
+    throw std::runtime_error("Not found value inside container");
+  }
+
+  return it;
+}
--- module-08/ex00/main.cpp
+++ module-08/ex00/main.cpp
@@ -1,5 +1,24 @@
+#include <iostream>
+#include <vector>
+
 #include "easyfind.hpp"
 
+void print(int i) {
+  std::cout << i << std::endl;
+}
+
 int main(void) {
-  std::cout << "Hello World" << std::endl;
+  std::vector<int> numbers(10);
+
+  for (int i = 0; i < 10; i++) {
+    numbers.push_back(i + 1);
+  }
+
+  try {
+    std::vector<int>::iterator it_vector = easyfind(numbers, 7);
+    std::cout << "Start print from find elem:" << std::endl;
+    for_each(it_vector, numbers.end(), print);
+  } catch (std::exception &err) {
+    std::cerr << "Error: " << err.what() << std::endl;
+  }
 }
