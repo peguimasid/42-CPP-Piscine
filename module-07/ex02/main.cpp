84fbb869b707c658b6cf35b08a428079c8e766f9
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-19 10:26:16 -0300

    improving tests

--- module-07/ex02/main.cpp
+++ module-07/ex02/main.cpp
@@ -4,11 +4,8 @@ int main(void) {
   int size = 10;
   Array<int> numbers(size);
 
-  for (int i = 0; i < size; i++) {
+  for (unsigned int i = 0; i < numbers.size(); i++) {
     numbers[i] = (i + 1) * 10;
-  }
-
-  for (int i = 0; i < size; i++) {
     std::cout << numbers[i] << std::endl;
   }
 }
