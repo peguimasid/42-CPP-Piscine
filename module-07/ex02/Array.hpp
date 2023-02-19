6aa2719d09530848a2623027c780af3e2837e969
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-19 10:20:05 -0300

    implement class

--- module-07/ex02/Array.hpp
+++ module-07/ex02/Array.hpp
@@ -9,13 +9,50 @@ class Array {
 
  public:
   // Constructors and Destructors
-  Array();
-  Array(unsigned int n);
-  Array(const Array &other);
-  Array &operator=(const Array &other);
-  ~Array();
+  Array() {
+    this->_array = new T[0];
+    this->size = 0;
+  }
+
+  Array(unsigned int n) {
+    this->_array = new T[n];
+    this->size = n;
+  }
+
+  Array(const Array &other) {
+    this->_array = new T[other.size];
+    this->size = other._size;
+    for (int i = 0; i < other._size; i++) {
+      this->_array[i] = other._array[i];
+    }
+  }
+
+  Array &operator=(const Array &other) {
+    if (this == &other) return *this;
+
+    this->_array = new T[other.size];
+    this->size = other._size;
+    for (int i = 0; i < other._size; i++) {
+      this->_array[i] = other._array[i];
+    }
+
+    return *this;
+  }
+
+  ~Array() {
+    delete[] this->_array;
+  }
 
   // Member functions
-  T &operator[](unsigned int index);
-  unsigned int size();
+  T &operator[](unsigned int index) {
+    if (index >= this->size) {
+      throw std::out_of_range("index is out of range");
+    }
+
+    return this->_array[index];
+  }
+
+  unsigned int size() {
+    return this->size;
+  }
 };
