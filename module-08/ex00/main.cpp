13b097e621c09d19d7e5c62434a1a2a58a23947c
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-19 12:34:30 -0300

    add more tests

--- module-08/ex00/main.cpp
+++ module-08/ex00/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <vector>
 
 #include "easyfind.hpp"
@@ -7,7 +8,7 @@ void print(int i) {
   std::cout << i << std::endl;
 }
 
-int main(void) {
+void test_1() {
   std::vector<int> numbers(10);
 
   for (int i = 0; i < 10; i++) {
@@ -21,4 +22,42 @@ int main(void) {
   } catch (std::exception &err) {
     std::cerr << "Error: " << err.what() << std::endl;
   }
+}
+
+void test_2() {
+  std::list<int> numbers(30);
+
+  for (int i = 0; i < 30; i++) {
+    numbers.push_back(i + 1);
+  }
+
+  try {
+    std::list<int>::iterator it_list = easyfind(numbers, 17);
+    std::cout << "Start print from find elem:" << std::endl;
+    for_each(it_list, numbers.end(), print);
+  } catch (std::exception &err) {
+    std::cerr << "Error: " << err.what() << std::endl;
+  }
+}
+
+void test_3() {
+  std::list<int> numbers(30);
+
+  for (int i = 0; i < 30; i++) {
+    numbers.push_back(i + 1);
+  }
+
+  try {
+    std::list<int>::iterator it_list = easyfind(numbers, 999);
+    std::cout << "Start print from find elem:" << std::endl;
+    for_each(it_list, numbers.end(), print);
+  } catch (std::exception &err) {
+    std::cerr << "Error: " << err.what() << std::endl;
+  }
+}
+
+int main(void) {
+  test_1();
+  test_2();
+  test_3();
 }
