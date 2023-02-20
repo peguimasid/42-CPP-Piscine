737dbe34440200ec83ea1f28044f909dc3efc223
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-20 14:26:00 -0300

    chenge function name

--- module-08/ex01/Span.hpp
+++ module-08/ex01/Span.hpp
@@ -16,7 +16,7 @@ class Span {
 
   // Member functions
   void addNumber(int num);
-  // void addNumber(const std::vector<int>::iterator& begin, const std::vector<int>::iterator& end);
+  // void addRange(const std::vector<int>::iterator& begin, const std::vector<int>::iterator& end);
   unsigned int shortestSpan();
   unsigned int longestSpan();
 
