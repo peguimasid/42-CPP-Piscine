259a818c9b6b0571c7ca2e30afacb3c89b9b28f4
Guilhermo Masid <guilhermomasid@gmail.com>
2023-05-08 18:33:31 -0300

    create main function and header file

---
+++ module-09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <map>
+
+class BitcoinExchange {
+ private:
+  std::map<std::string, float> _map;
+
+ public:
+  // Constructors and Destructors
+  BitcoinExchange();
+  BitcoinExchange(const BitcoinExchange &other);
+  BitcoinExchange &operator=(const BitcoinExchange &other);
+  ~BitcoinExchange();
+};
---
+++ module-09/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  std::cout << "Hello World" << std::endl;
+}
