2bc4993bc5d746453f7db120e140868e0101830c
Guilhermo Masid <guilhermomasid@gmail.com>
2023-05-08 19:02:54 -0300

    handle nonexitent file and empty file

--- module-09/ex00/main.cpp
+++ module-09/ex00/main.cpp
@@ -10,5 +10,11 @@ int error(std::string errorMessage) {
 int main(int argc, char **argv) {
   if (argc != 2) return error("Error: could not open file.");
   std::string filename(argv[1]);
-  std::cout << filename << std::endl;
+  std::ifstream inputFile(filename);
+  if (!inputFile.is_open()) {
+    return error("Error: could not open file.");
+  }
+  if (inputFile.peek() == std::ifstream::traits_type::eof()) {
+    return error("Error: File is empty");
+  }
 }
