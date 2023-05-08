f7235d66132fb036e9072c5c42cb2843ac58bee2
Guilhermo Masid <guilhermomasid@gmail.com>
2023-05-08 18:59:01 -0300

    handle wrong num of arguments

--- module-09/ex00/main.cpp
+++ module-09/ex00/main.cpp
@@ -1,7 +1,14 @@
 #include "BitcoinExchange.hpp"
 
+int error(std::string errorMessage) {
+  std::cout << "\033[0;31m";
+  std::cout << errorMessage << std::endl;
+  std::cout << "\033[0m";
+  return 1;
+}
+
 int main(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
-  std::cout << "Hello World" << std::endl;
+  if (argc != 2) return error("Error: could not open file.");
+  std::string filename(argv[1]);
+  std::cout << filename << std::endl;
 }
