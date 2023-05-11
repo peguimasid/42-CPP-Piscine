147b66548d7ac053c47580c01ebdb251718bafe0
Guilhermo Masid <guilhermomasid@gmail.com>
2023-05-11 19:07:46 -0300

    create function to process exchange file

--- module-09/ex00/BitcoinExchange.cpp
+++ module-09/ex00/BitcoinExchange.cpp
@@ -28,12 +28,15 @@ void BitcoinExchange::parse_data_file() {
   while (getline(inputFile, currentLine)) {
     std::string date = currentLine.substr(0, 10).erase(4, 1).erase(6, 1);
     float rate = atof(currentLine.substr(11).c_str());
-    std::cout << rate << std::endl;
     this->_map.insert(std::make_pair(date, rate));
   }
 }
 
+void process_exchange_file(const std::string &filename) {
+  std::cout << filename << std::endl;
+}
+
 void BitcoinExchange::execute(const std::string &filename) {
-  (void)filename;
   parse_data_file();
+  process_exchange_file(filename);
 }
